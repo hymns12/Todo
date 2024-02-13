91630b21abd8935fd0c76f81d86790eddaaa5d66
hymns12 <nickcloudsyinka1@gmail.com>
2024-02-13 10:03:25 -0500

    first commit

--- contracts/Todos.sol
+++ contracts/Todos.sol
@@ -9,6 +9,22 @@ contract Todos {
         string user;
         bool isDone;
     }
+    Todo[] public ArrayTodo;
 
+    function craetTodo(uint _id, string memory _name, string memory _user) external {
+        Todo memory todo;
+        todo.id = _id;
+        todo.name = _name;
+        todo.user = _user;
+
+        ArrayTodo.push(todo);
+    }
+
+    function getTodo(uint _index) external view returns(string memory _name, uint _id, bool isDone, string memory _user) {
+        Todo storage todo = ArrayTodo[_index];
+        return (todo.name, todo.id, todo.isDone, todo.user);
+    }
+
+    
     
 }

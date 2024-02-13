474df93cb23791976c3dd724bc730ac77693064c
hymns12 <nickcloudsyinka1@gmail.com>
2024-02-13 08:11:37 -0500

    first commit

---
+++ contracts/Todos.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Todos {
+
+    struct Todo {
+        uint id;
+        string name;
+        string user;
+        bool isDone;
+    }
+
+    
+}
